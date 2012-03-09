8f1cf2bc00a9b382562e41f9622c3cb8c1c2e15a
Tatsh <ddrtist@gmail.com>
2012-03-09 00:01:34 -0600

    Move to sDocument class

--- sDocument.js
+++ sDocument.js
@@ -67,12 +67,69 @@ sDocument.prototype.byId = function (id) {
   return document.getElementById(id);
 };
 /**
- * Gets elements by class name.
+ * Gets elements by class name. Based on work by Robert Nyman.
  * @param {string} className Class name.
  * @returns {NodeList|Array} Element node list.
+ * @see http://code.google.com/p/getelementsbyclassname/
  */
 sDocument.prototype.byClassName = function (className) {
-  return sDependencies.getElementsByClassName(className);
+  var elm = document;
+  var ret, doc = document, classesToCheck, classes = className.split(' '), j;
+  var elements;
+
+  if (elm.getElementsByClassName) {
+    ret = elm.getElementsByClassName(className);
+  }
+  else if (doc.evaluate) {
+    // do XPath query and return array
+    var xhtmlNamespace = 'http://www.w3.org/1999/xhtml';
+    var namespaceResolver = doc.documentElement.namespaceURI === xhtmlNamespace ? xhtmlNamespace : null;
+    var node;
+
+    ret = [];
+    classesToCheck = '';
+
+    for (j = 0; j < classes.length; j++) {
+      classesToCheck += '[contains(concat(" ", @class, " "), " ' + classes[j] + ' ")]';
+    }
+
+    try {
+      elements = doc.evaluate('.//*' + classesToCheck, elm, namespaceResolver, 0, null);
+    }
+    catch (e) {
+      elements = doc.evaluate('.//*' + classesToCheck, elm, null, 0, null);
+    }
+
+    while ((node = elements.iterateNext())) {
+      ret.push(node);
+    }
+  }
+  else {
+    var match, k;
+
+    ret = [];
+    elements = elm.all ? elm.all : elm.getElementsByTagName('*');
+    classesToCheck = [];
+
+    for (j = 0; j < classes.length; j++) {
+      classesToCheck.push(new RegExp('(\\s+)?' + classes[j] + '(\\s+)?'));
+    }
+
+    for (j = 0; j < elements.length; j++) {
+      match = false;
+      for (k = 0; k < classesToCheck.length; k++) {
+        if (classesToCheck[k].test(elements[j].className)) {
+          match = true;
+          break;
+        }
+      }
+      if (match) {
+        ret.push(elements[j]);
+      }
+    }
+  }
+
+  return ret;
 };
 /**
  * Create a new element.
